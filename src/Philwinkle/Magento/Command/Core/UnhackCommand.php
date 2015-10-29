c53c6c3f99fa35723f83e5adab9c02fd036bb35d
Kalen Jordan <kalen@magemail.co>
2015-10-29 11:40:32 -0700

    Hook the detect class up to unhack command input

--- src/Philwinkle/Magento/Command/Core/UnhackCommand.php
+++ src/Philwinkle/Magento/Command/Core/UnhackCommand.php
@@ -2,20 +2,22 @@
 
 namespace Philwinkle\Magento\Command\Core;
 
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
-use KJ\Magento\Util\Comparison\Item;
+use Philwinkle\Detect;
 
 class UnhackCommand extends \N98\Magento\Command\AbstractMagentoCommand
 {
 	protected $_version = null;
 
+	/** @var InputInterface */
+	protected $_input;
+
 	protected function configure()
 	{
-		$this
-			->setName('core:unhack')
+		$this->setName('core:unhack')
+			->addArgument('files', InputArgument::REQUIRED, 'The list of file names and line numbers')
 			->setDescription('Unhack the core');
 	}
 
@@ -24,6 +26,32 @@ class UnhackCommand extends \N98\Magento\Command\AbstractMagentoCommand
 		$this->_input = $input;
 		$this->_output = $output;
 
-		$output->writeln("Do eeet");
+		$this->detectMagento($output);
+		$this->initMagento();
+
+		$lines = $this->_parseLines();
+		foreach ($lines as $lineNumber => $filePath) {
+			$file = new \SplFileObject($filePath, "r");
+			$detect = new Detect($file, $lineNumber);
+			print_r($detect);
+		}
+	}
+
+	protected function _parseLines()
+	{
+		$input = $this->_input->getArgument('files');
+		$lines = explode("\r\n", $input);
+
+		$results = array();
+		foreach ($lines as $line) {
+			$parts = explode(":", $line);
+			if (count($parts) != 2) {
+				continue;
+			}
+
+			$results[$parts[1]] = $parts[0];
+		}
+
+		return $results;
 	}
 }
