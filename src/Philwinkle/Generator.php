77c5a6aa449e530525f468dd0157bc23ebc2e162
Phillip Jackson <philwinkle@gmail.com>
2015-10-29 22:27:39 -0400

    Add generator class type

---
+++ src/Philwinkle/Generator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Philwinkle;
+
+class Generator
+{
+    public static function run($hacks)
+    {
+        foreach($hacks as $className=>$hack){
+            
+        }
+    }
+}
